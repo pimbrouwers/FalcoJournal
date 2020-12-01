c368e9efd2b71f781161ed7e37658341e2bf69c2
Pim Brouwers <pimnation@gmail.com>
2020-12-01 06:48:23 -0500

    project, packages, html prototype

---
+++ src/FalcoJournal/Program.fs
@@ -0,0 +1,56 @@
+module FalcoJournal.Program
+
+open Falco
+open Falco.Routing
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+// ------------
+// Handlers 
+// ------------
+let handlePlainText : HttpHandler =
+    Response.ofPlainText "Hello from /"
+
+// ------------
+// Routes
+// ------------
+let endpoints =
+    [            
+        get "/" handlePlainText
+    ]
+
+// ------------
+// Register services
+// ------------
+let configureServices (services : IServiceCollection) =
+    services.AddFalco() |> ignore
+
+// ------------
+// Activate middleware
+// ------------
+let configureApp (ctx : WebHostBuilderContext) (app : IApplicationBuilder) =    
+    let devMode = StringUtils.strEquals ctx.HostingEnvironment.EnvironmentName "Development"    
+    app.UseWhen(devMode, fun app -> 
+            app.UseDeveloperExceptionPage())
+       .UseWhen(not(devMode), fun app -> 
+            app.UseFalcoExceptionHandler(Response.withStatusCode 500 >> Response.ofPlainText "Server error"))
+       .UseFalco(endpoints) |> ignore
+
+[<EntryPoint>]
+let main args =    
+    try
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(fun webhost ->   
+                webhost
+                    .ConfigureServices(configureServices)
+                    .Configure(configureApp)
+                    |> ignore)
+            .Build()
+            .Run()                        
+        0
+    with 
+    | ex -> 
+        printfn "%s\n\n%s" ex.Message ex.StackTrace
+        -1
